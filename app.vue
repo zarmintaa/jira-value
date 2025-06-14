06950a112893ca752d774a73e38e705554cebdee
HuaaXZ <59224888+zarmintaa@users.noreply.github.com>
2025-06-14 11:08:49 +0700

    initial commit add analytics

--- app.vue
+++ app.vue
@@ -1,10 +1,15 @@
+<script lang="ts" setup>
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
+
 <template>
   <div>
+    <Analytics />
+
     <NuxtRouteAnnouncer />
     <NuxtLayout>
       <NuxtLoadingIndicator :height="3" :duration="3000" :throttle="100" />
       <NuxtPage />
-
       <ErrorNotificationList />
     </NuxtLayout>
   </div>
